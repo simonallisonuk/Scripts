f2c860c1f704e250865f72030bc968eee251c12d
simonallisonuk <simonallisonuk@outlook.com>
2019-11-05 01:04:56 +0000

    Update GetAutoPilot.cmd

--- GetAutoPilot.cmd
+++ GetAutoPilot.cmd
@@ -1,1 +1,4 @@
+//https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/1.6
+
+
 PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~dp0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~dp0\computers.csv -append
