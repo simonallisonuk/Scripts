6a3e72b971bed3df257868b3f06e92703fb21db4
simonallisonuk <simonallisonuk@outlook.com>
2019-11-08 10:12:06 +0000

    Update
    %~dp0 gives full path where as %~d0 gives drive letter  - updated script

--- GetAutoPilot.cmd
+++ GetAutoPilot.cmd
@@ -1,4 +1,3 @@
 REM Download this powershell script and inclde with this cmd file - https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/1.6
 
-
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~dp0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~dp0\computers.csv -append
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~d0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~d0\computers.csv -append
