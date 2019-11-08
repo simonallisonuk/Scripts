354f6ff360c885ce3c6c95b29ed7f6225ba085a7
simonallisonuk <simonallisonuk@outlook.com>
2019-11-08 10:13:44 +0000

    added comment

--- GetAutoPilot.cmd
+++ GetAutoPilot.cmd
@@ -1,3 +1,4 @@
 REM Download this powershell script and inclde with this cmd file - https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/1.6
+REM This needs winrm running on an existing system : winrm quickconfig 
 
 PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~d0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~d0\computers.csv -append
